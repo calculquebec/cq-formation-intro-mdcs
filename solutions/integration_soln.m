4a97319363e372aebe0713d4e8fb43a16085f7f4
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2015-12-03 11:05:40 -0500

    Improved integration solution

--- solutions/integration_soln.m
+++ solutions/integration_soln.m
@@ -1,16 +1,21 @@
-    tic
+tic
 spmd
     m = 100000;
     a = 0;
     b = pi/2;
+    dx = (b-a)/m;
+
     n = m/numlabs;
-    Delta = (b-a)/numlabs;
+    Delta = (b-a)/numlabs;    % dx * n
+
     ai = a + (labindex - 1)*Delta;
     bi = a + labindex*Delta;
-    dx = (b-a)/m;
+
     x = ai+dx/2:dx:bi-dx/2;
+
     int = sum(cos(x)*dx);
     int = gplus(int, 1);
 end
-disp(1-int{1})
 toc
+
+disp(1-int{1})
